c30cdad6c2c753b2f2d4c104bb43e6090cb777e4
Lynsey Powell <lynseympowell@gmail.com>
2018-06-02 09:04:07 -0600

    added repl.it files

---
+++ datastore.js
@@ -0,0 +1,52 @@
+const questionArray = [
+  {
+  question: 'What estate did Lizzy visit while on vacation  with her aunt and uncle?',
+  answers: ['Pemberly', 'Longbourn', 'Downton', 'Netherfield Park'],
+  correct: 0
+},
+{
+  question: 'What movie is a modern day Pride & Prejudice?',
+  answers: [ 'You\'ve Got Mail', 'How to be Single', 'Bridget Jones\' Diary', 'Notting Hill'],
+  correct: 2
+},
+{
+question: 'Who narrowly escapes becoming an old maid by marrying Mr. Collins?',
+answers: [ 'Elizabeth Bennett', 'Charlotte Lucas','Lydia Bennett', 'Anne de Bourgh'],
+correct: 1
+},
+{
+  question:'What military officer tarnishes the Bennett name by eloping with one of the sisters?',
+  answers: [ 'Denny', 'Mr. Wickham', 'Mr. Darcy', 'Col. Fitzwilliam'],
+  correct: 1
+},
+{
+  question: 'According to her mother, Anne de Bourgh is destined to marry which gentleman?',
+  answers: [ 'Col. Fitzwilliam', 'Mr. Collins', 'Mr. Bingley', 'Mr. Darcy'],
+  correct: 3
+},
+{
+  question: 'How many proposals does Elizabeth Bennett receive in the book?',
+  answers: [3, 4, 2, 1],
+  correct: 0
+},
+{
+  question: 'What malady confines Mrs. Bennett to her room?',
+  answers: [ 'Pneumonia', 'Consumption', 'Fainting', 'Nerves'],
+  correct: 3
+},
+{
+  question: 'What is Mr. Darcy\'s first name?',
+  answers: ['Mark', 'Fitzwilliam', 'Charles', 'Edward'],
+  correct: 1
+},
+{
+  question: 'What are the names of the five Bennett sisters?',
+  answers: ['Jane, Elizabeth, Mary, Kitty, Lydia', 'Charlotte, Elizabeth, Lydia, Georgiana, Mary', 'Caroline, Jane, Catherine, Kitty, Anne', 'Anne, Elizabeth, Mary, Rebecca, Sarah'],
+  correct: 0
+},
+{
+  question: 'A recent adaptation of Pride & Prejudice includes what unlikely character\'s?',
+  answers: [ 'Vampires', 'Fairies', 'Zombies','Mr. & Mrs Claus'],
+  correct: 2
+}];
+
---
+++ index.js
@@ -0,0 +1,169 @@
+'use strict';
+
+let playerScore = 0;
+let gameProgress = 0;
+
+//create score HTML
+function generateScoreString() {
+  const playerScoreString = `Score: ${playerScore}`;
+  $('.current-score').html(playerScoreString);
+}
+
+//create string to show current question number
+function generateProgressString() {
+  const gameProgressString = `Question: ${gameProgress}/10`;
+  $('.progress').html(gameProgressString);
+}
+
+// didn't fix it by putting it in a function
+//function resetScoreProgress() {
+ // let playerScore = 0;
+  //let gameProgress = 0;
+// }
+
+//populate progress and score on start page
+function renderStartPage() {
+  generateScoreString();
+  generateProgressString();
+  $('h2').removeClass('hidden');
+  $('div.finalResult').html('');
+  $('form').removeClass('add-background');
+  $('.content').removeClass('hidden');
+  $('.form-h3').html('Put on a pot of tea and let\'s find out!').removeClass('hidden');
+  $('.proceed-button').removeClass('hidden view-results').html('Commence');
+  console.log('render start page ran');
+}
+
+//increase the current question number displayed
+function increaseProgress() {
+  gameProgress++;
+  generateProgressString();
+}
+
+function increaseScore() {
+  playerScore++;
+  generateScoreString();
+}
+
+//hide or show relevant html
+function toggleHiddenClass() {
+  $('.results').toggleClass('hidden');
+  $('fieldset').toggleClass('hidden');
+  $('.proceed-button').toggleClass('hidden');
+  // $('.answer-button').toggleClass('hidden');
+}
+
+//remove <h3>, show & populate fieldset, toggle buttons
+function generateQuestionElement(questionObject) {
+  $('.form-h3').addClass('hidden'); //move to handleCommenceNextButton?
+  $('.proceed-button').html('Next');//move to handleCommenceNextButton?
+  toggleHiddenClass(); //move to renderQuestion? or HCNB?
+  return ` 
+      <legend class="question">${questionObject.question}</legend>
+       <label for="answer1">
+      <input type="radio" name="answer" id="answer1" value=0 required>
+       ${questionObject.answers[0]}</label>
+      <label for="answer2">
+      <input type="radio" name="answer" id="answer2" value=1 required>
+      ${questionObject.answers[1]}</label>
+      <label for="answer3">
+      <input type="radio" name="answer" id="answer3" value=2 required>
+      ${questionObject.answers[2]}</label>
+      <label for="answer4">
+      <input type="radio" name="answer" id="answer4" value=3 required>
+      ${questionObject.answers[3]}</label> 
+      <button type="submit" class="answer-button">Submit</button>`;
+}
+
+//display the question content and increase progress display
+function renderQuestion() {
+  //increase gameProgress after assigning index
+  const questionIndex = gameProgress;
+  increaseProgress();
+  //get html string for question set
+  const currentQuestion = generateQuestionElement(questionArray[questionIndex]);
+  $('fieldset').html(currentQuestion);
+}
+
+function displayFinalResults() {
+      $('div.content').addClass('hidden');
+      if (playerScore < 7){
+         $('div.finalResult').html(`<h3>${playerScore} out of 10, <br>time to watch Pride and Prejudice again!</h3><img src="https://cdn3.iconfinder.com/data/icons/watercolorcafe/128/teacup.png" alt="A teacup"><button type="submit" class="startover">Start Over</button>`);
+      } else {
+         $('div.finalResult').html(`<h3>Elizabeth is proud!<br> You scored ${playerScore} out of 10. </h3><img src="https://cdn3.iconfinder.com/data/icons/watercolorcafe/128/teacup.png" alt="A dainty teacup drawing"><button type="submit" class="startover">Start Over</button>`);
+      }
+  }
+
+//listens for button click to move to first question
+//after last question display results
+function handleCommenceNextButton() {
+  $('.question-form').on('click', '.proceed-button', function(event) { 
+    event.preventDefault();
+    $('form').addClass('add-background');
+    $('h2').addClass('hidden');
+    if (gameProgress < questionArray.length) {
+      renderQuestion();
+      $('.results').addClass('hidden'); 
+    } else {
+      $('.results').addClass('hidden');
+      displayFinalResults();
+  }
+  });
+}
+
+//move if to correct/incorrect 
+function displayCorrectResult() {
+   toggleHiddenClass();
+  $('.results').html('<h3>Capital! Capital!<br> You\'re making fine progress.</h3>');
+   if (gameProgress == questionArray.length) {
+     $('.proceed-button').html('View Results');
+     $('.proceed-button').addClass('view-results');
+   }
+}
+
+function displayIncorrectResult(rightAnswer) {
+   toggleHiddenClass();
+  $('.results').html(`<h3>Incorrect!<br> The correct answer is ${(questionArray[gameProgress - 1]).answers[rightAnswer]}.</h3>`); 
+  if (gameProgress == questionArray.length) {
+     $('.proceed-button').html('View Results');
+     $('.proceed-button').addClass('view-results');
+   }
+}
+
+function compareAnswer () {
+  let rightAnswer = `${(questionArray[gameProgress-1]).correct}`
+  let selected = $('input:checked').val();
+  if (rightAnswer == selected) {
+    displayCorrectResult();
+    increaseScore();
+  } else {
+    displayIncorrectResult(rightAnswer);
+  }
+}
+
+function handleAnswerButton() {
+  $('.question-form').on('submit', function
+  (event) {
+    event.preventDefault();
+    compareAnswer();
+  });
+}
+
+function handleStartOverButton() {
+  $('.finalResult').on('click', '.startover', function(event) { 
+    event.preventDefault();
+    playerScore = 0;
+    gameProgress = 0;
+    renderStartPage();
+  });
+}
+
+function runQuiz() {
+  renderStartPage();
+  handleCommenceNextButton();
+  handleAnswerButton();
+  handleStartOverButton();
+
+}
+
+$(runQuiz);
